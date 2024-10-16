eb147f17a0459ede4c0bb09193789fea24765eba
Julio Avello <julioavello94@iesamachado.org>
2024-10-16 13:50:28 +0200

    inicio defcon 1

---
+++ Primer trimestre/Artista/pruebesita.js
@@ -0,0 +1,20 @@
+let fecha = 2003 ;
+let userInput = 0 ;
+let intentos = 0 ;
+let salida = "Hola mundo";
+
+function adivinar() {
+    userInput = document.getElementById("respuesta").value;
+
+    if (userInput !== fecha) {
+
+        salida = '${userInput} no es correcto.';
+        intentos ++;
+    }
+
+    if (userInput == fecha) {
+        salida ='${userInput} es correcto.';
+    }
+
+    document.getElementById("intentos").innerHTML = '${salida}'
+}
